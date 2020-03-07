b8f9ffed4fedf19fab610ee8815e980b94ce5894
emc2314 <emc2314@mail.ustc.edu.cn>
2020-03-07 15:50:13 +0800

    Update constant

--- lib/Transforms/Obfuscate/Util.h
+++ lib/Transforms/Obfuscate/Util.h
@@ -3,7 +3,7 @@
 
 void fixStack(llvm::Function *f);
 
-const uint32_t fnvPrime = 16777633;
+const uint32_t fnvPrime = 19260817;
 const uint32_t fnvBasis = 0x114514;
 uint32_t fnvHash(const uint32_t data, uint32_t b);
 llvm::InlineAsm *generateGarbage(llvm::Function *f);
